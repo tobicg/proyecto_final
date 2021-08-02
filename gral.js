5514862f899f4deec070571884e66a93b819ab6b
tobicg <tobicareaga@gmail.com>
2021-08-02 19:54:20 -0300

    commitjs

--- gral.js
+++ gral.js
@@ -1,5 +1,5 @@
 /* index */
-ScrollReveal().reveal('.main-index-nosotros-div-txt-p, .main-index-nosotros-div-txt-a, .main-index-sucursales-div-img', { delay: 300});
+ScrollReveal().reveal('.main-index-nosotros-div-txt-p, .main-index-nosotros-div-txt-a, .main-index-sucursales-div-img', { delay: 100});
 
 /* carta */
 ScrollReveal().reveal('.main-carta-fondo-burgers-item, .main-carta-fondo-acompanamientos-item, .main-carta-fondo-pizzas-item', { delay: 100});
