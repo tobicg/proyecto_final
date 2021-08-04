d0afc762c00b6a7922a882b77bed2f7ef142b7ca
tobicg <tobicareaga@gmail.com>
2021-08-04 20:03:39 -0300

    commit-mod-nosotros

--- gral.js
+++ gral.js
@@ -3,3 +3,6 @@ ScrollReveal().reveal('.main-index-nosotros-div-txt-p, .main-index-nosotros-div-
 
 /* carta */
 ScrollReveal().reveal('.main-carta-fondo-burgers-item, .main-carta-fondo-acompanamientos-item, .main-carta-fondo-pizzas-item', { delay: 100});
+
+/* nosotros */
+ScrollReveal().reveal('.nuestracomida-div-title, .nuestracomida-div-txt, .nuestroproceso-div-title, .nuestroproceso-div-txt', { delay: 100});
